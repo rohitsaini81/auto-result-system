f978f08166151091fb3207e36a0fc1aaf0e421e3
rohit <rohit@kali.kali>
2023-12-04 18:16:58 +0530

    cdn null fixes

--- domcdn.js
+++ domcdn.js
@@ -2,44 +2,44 @@
     const response = await fetch("https://auto-system.onrender.com/api/6");
     const data = await response.json();
     console.log(data);
-    <!--const out = document.getElementById('OUT')-->
+    // <!--const out = document.getElementById('OUT')-->
     const dl = document.getElementById('_DL');
     const sg = document.getElementById('_SG');
     const ds = document.getElementById('_DS');
     const fd = document.getElementById('_FD');
     const gl = document.getElementById('_GL');
     const gz = document.getElementById('_GZ');
-    <!--yesterday result id-->
+    // <!--yesterday result id-->
     const o_dl = document.getElementById('O_DL');
     const o_sg = document.getElementById('O_SG');
     const o_ds = document.getElementById('O_DS');
     const o_fd = document.getElementById('O_FD');
     const o_gl = document.getElementById('O_GL');
     const o_gz = document.getElementById('O_GZ');
-    <!--out.innerText += data.map((item) => item.today).join(' ');-->
+    // <!--out.innerText += data.map((item) => item.today).join(' ');-->
     // console.log(data[0].today)
     for (let i = 0; i < data.length; i++) {
-        if (data[i].name == 'delhi bazar') {
+        if (data[i].name == 'delhi bazar' && data[i].today > 5) {
             dl.innerText = data[i].today;
             o_dl.innerText = data[i].yesterday;
         }
-        if (data[i].name == 'shri ganesh') {
+        if (data[i].name == 'shri ganesh' && data[i].today > 5) {
             sg.innerText = data[i].today;
             o_sg.innerText = data[i].yesterday;
         }
-        if (data[i].name == 'disawar') {
+        if (data[i].name == 'disawar' && data[i].today > 5) {
             ds.innerText = data[i].today;
             o_ds.innerText = data[i].yesterday
         }
-        if (data[i].name == 'faridabad') {
+        if (data[i].name == 'faridabad' && data[i].today > 5) {
             fd.innerText = data[i].today;
             o_fd.innerText = data[i].yesterday;
         }
-        if (data[i].name == 'gali') {
+        if (data[i].name == 'gali' && data[i].today > 5) {
             gl.innerText = data[i].today;
             o_gl.innerText = data[i].yesterday;
         }
-        if (data[i].name == 'gaziyabad') {
+        if (data[i].name == 'gaziyabad' && data[i].today > 5) {
             gz.innerText = data[i].today;
             o_gz.innerText = data[i].yesterday;
         }
